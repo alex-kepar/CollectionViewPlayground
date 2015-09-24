e897204b3c3ae1c8b523b308881d10c7aa2afdb9
Oleksandr Kiporenko <oleksandr_kiporenko@epam.com>
2015-09-24 19:04:35 +0300

    add files

---
+++ CollectionViewPlayground/uiCollectionViewMy.h
@@ -0,0 +1,13 @@
+//
+//  uiCollectionViewMy.h
+//  CollectionViewPlayground
+//
+//  Created by Oleksandr Kiporenko on 9/24/15.
+//  Copyright (c) 2015 QVC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICollectionViewMy : UICollectionView
+
+@end
---
+++ CollectionViewPlayground/uiCollectionViewMy.m
@@ -0,0 +1,22 @@
+//
+//  uiCollectionViewMy.m
+//  CollectionViewPlayground
+//
+//  Created by Oleksandr Kiporenko on 9/24/15.
+//  Copyright (c) 2015 QVC. All rights reserved.
+//
+
+#import "UICollectionViewMy.h"
+
+@implementation UICollectionViewMy
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    
+    if (![self pointInside:point withEvent:event])
+    {
+        self.hidden = YES;
+    }
+    return [super hitTest:point withEvent:event];
+}
+
+@end
